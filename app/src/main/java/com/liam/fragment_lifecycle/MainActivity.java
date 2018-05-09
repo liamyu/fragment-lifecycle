4241b04682827d578f571eb31fd9e4269211fc63
DESKTOP-HGH05A8\Liam <ylc@cang.com>
2018-05-09 15:00:58 +0800

    initial commit.

---
+++ app/src/main/java/com/liam/fragment_lifecycle/MainActivity.java
@@ -0,0 +1,63 @@
+package com.liam.fragment_lifecycle;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements
+    BlankFragment.OnFragmentInteractionListener {
+
+  private static final String TAG = "main:blank_fragment";
+  private static final String BTN_TXT = "main:btn_txt";
+  private BlankFragment fragment;
+  private Button btn;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    if (savedInstanceState != null) {
+      fragment = (BlankFragment) getSupportFragmentManager().findFragmentByTag(TAG);
+    }
+    btn = findViewById(R.id.button);
+    btn.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if (fragment != null) {
+          ft.remove(fragment).commit();
+          fragment = null;
+          btn.setText("添加Fragment");
+        } else {
+          fragment = BlankFragment.newInstance("", "");
+          ft.add(R.id.container, fragment, TAG).commit();
+          btn.setText("移除Fragment");
+        }
+      }
+    });
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putString(BTN_TXT, btn.getText().toString());
+  }
+
+  @Override
+  protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    btn.setText(savedInstanceState.getString(BTN_TXT));
+  }
+
+  @Override
+  public void onFragmentInteraction(Uri uri) {
+
+  }
+}
